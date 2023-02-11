5231df93106566cdb23031c77752aea007835142
Alexey Sokolov <alexvyber@gmail.com>
2023-02-11 10:07:43 +0300

    init + 1-3 videos finished

---
+++ app.ts
@@ -0,0 +1,76 @@
+// eslint-disable-next-line
+require("dotenv").config()
+import express from "express"
+import { graphqlHTTP } from "express-graphql"
+import { makeExecutableSchema } from "@graphql-tools/schema"
+import { PrismaClient } from "@prisma/client"
+
+const { random } = Math
+
+const app = express()
+const port = 3000
+const prisma = new PrismaClient()
+
+const typeDefs = `
+  type File {
+    id: String!
+    name: String!
+    directoryId: String!
+    createdAt: String!
+    updatedAt: String!
+    versions: [FileVersion]!
+  }
+
+  type FileVersion {
+    id: String!
+    name: String!
+    fileId: String!
+    mimeType: String!
+    size: Int!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  type Directory {
+    id: String!
+    name: String!
+    parentId: String
+    createdAt: String!
+    updatedAt: String!
+    files: [File]!
+    directories: [Directory]!
+  }
+
+  type Query {
+    getAllFiles: [File]!
+    getAllFileVersions: [FileVersion]!
+    getAllDirectories: [Directory]!
+    someShit: [String]!
+  }
+`
+
+const resolvers = {
+  Query: {
+    getAllFiles: () => prisma.file.findMany(),
+    getAllFileVersions: () => prisma.fileVersion.findMany(),
+    getAllDirectories: () => prisma.directory.findMany(),
+    someShit: () => (random() > 0.5 ? ["some", "shit"] : ["other", "shit"]),
+  },
+}
+
+export const schema = makeExecutableSchema({
+  resolvers,
+  typeDefs,
+})
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+)
+
+app.listen(port, () => {
+  console.log(`Application running on port ${port}.`)
+})
